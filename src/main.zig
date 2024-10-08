88c71da6ce6261119b8ade732d77bb71356cbc8f
sidharta <jeansidharta@gmail.com>
2024-10-08 19:16:42 -0400

    Added file input/output

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,38 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // const gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = gpa.allocator();
+    var stdargs = std.process.args();
+    // Skip program name
+    _ = stdargs.next();
+    const inputFilePath = stdargs.next() orelse "-";
+    const outputFilePath = stdargs.next() orelse "-";
 
-    const stdinReader = std.io.getStdIn().reader();
-    const stdoutWriter = std.io.getStdOut().writer();
+    const fileReader = blk: {
+        if (std.mem.eql(u8, inputFilePath, "-")) {
+            break :blk std.io.getStdIn().reader();
+        } else {
+            break :blk (try std.fs.cwd().openFile(inputFilePath, .{ .mode = .read_only })).reader();
+        }
+    };
+    const fileWriter = blk: {
+        if (std.mem.eql(u8, outputFilePath, "-")) {
+            break :blk std.io.getStdOut().writer();
+        } else {
+            break :blk (try std.fs.cwd().createFile(outputFilePath, .{})).writer();
+        }
+    };
 
     var isCode = false;
     var buffer: [16512]u8 = undefined;
-    while (true) {
-        const line = (try stdinReader.readUntilDelimiterOrEof(&buffer, '\n')) orelse break;
+    while (try fileReader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
         const trimmedLine = line[std.mem.indexOfNone(u8, line, &[_]u8{ ' ', '\t' }) orelse 0 ..];
         if (std.mem.startsWith(u8, trimmedLine, "```")) {
             isCode = !isCode;
             continue;
         }
         if (isCode) {
-            _ = try stdoutWriter.write(line);
-            _ = try stdoutWriter.write("\n");
+            _ = try fileWriter.write(line);
+            _ = try fileWriter.write("\n");
         }
     }
 }
