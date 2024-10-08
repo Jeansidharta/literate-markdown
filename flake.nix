855339485a1b0bdb4fcd4b7caf3a8aea06e20bd9
sidharta <jeansidharta@gmail.com>
2024-10-08 18:52:33 -0400

    Added packages output to nix flake

--- flake.nix
+++ flake.nix
@@ -12,9 +12,14 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        package-name = "literate-markdown";
       in
       {
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ zig ]; };
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.zig ]; };
+        packages.default = pkgs.runCommand package-name { } ''
+          mkdir $out/bin $out/lib -p
+          ${pkgs.zig}/bin/zig build-exe -OReleaseFast -Mroot=${./src/main.zig} --cache-dir $TEMP --global-cache-dir $TEMP --name ${package-name} -femit-bin=$out/bin/${package-name}
+        '';
       }
     );
 }
