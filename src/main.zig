a5a8243fffb422c9c57ac15df3af18fa21898217
sidharta <jeansidharta@gmail.com>
2024-10-08 19:27:00 -0400

    Added proper file close

--- src/main.zig
+++ src/main.zig
@@ -4,23 +4,30 @@ pub fn main() !void {
     var stdargs = std.process.args();
     // Skip program name
     _ = stdargs.next();
+
     const inputFilePath = stdargs.next() orelse "-";
     const outputFilePath = stdargs.next() orelse "-";
 
-    const fileReader = blk: {
+    const inputFile = blk: {
         if (std.mem.eql(u8, inputFilePath, "-")) {
-            break :blk std.io.getStdIn().reader();
+            break :blk std.io.getStdIn();
         } else {
-            break :blk (try std.fs.cwd().openFile(inputFilePath, .{ .mode = .read_only })).reader();
+            break :blk try std.fs.cwd().openFile(inputFilePath, .{ .mode = .read_only });
         }
     };
-    const fileWriter = blk: {
+    defer inputFile.close();
+
+    const outputFile = blk: {
         if (std.mem.eql(u8, outputFilePath, "-")) {
-            break :blk std.io.getStdOut().writer();
+            break :blk std.io.getStdOut();
         } else {
-            break :blk (try std.fs.cwd().createFile(outputFilePath, .{})).writer();
+            break :blk try std.fs.cwd().createFile(outputFilePath, .{});
         }
     };
+    defer outputFile.close();
+
+    const fileReader = inputFile.reader();
+    const fileWriter = outputFile.writer();
 
     var isCode = false;
     var buffer: [16512]u8 = undefined;
