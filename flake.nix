5b26863ec412ba663d4c2ee4c50702b9937e4d05
sidharta <jeansidharta@gmail.com>
2025-10-16 08:23:12 -0400

    Add zig2nix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,91 @@
 {
+  description = "Zig project flake";
+
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    zig2nix.url = "github:Cloudef/zig2nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      utils,
-    }:
-    utils.lib.eachDefaultSystem (
+    { zig2nix, nixpkgs, ... }:
+    let
+      flake-utils = zig2nix.inputs.flake-utils;
+    in
+    (flake-utils.lib.eachDefaultSystem (
       system:
       let
+        # Zig flake helper
+        # Check the flake.nix in zig2nix project for more options:
+        # <https://github.com/Cloudef/zig2nix/blob/master/flake.nix>
+        env = zig2nix.outputs.zig-env.${system} { };
         pkgs = nixpkgs.legacyPackages.${system};
-        package-name = "literate-markdown";
       in
-      {
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.zig pkgs.zls ]; };
-        packages.default = pkgs.runCommand package-name { } ''
-          mkdir $out/bin $out/lib -p
-          ${pkgs.zig}/bin/zig build-exe -OReleaseFast -Mroot=${./src/main.zig} --cache-dir $TEMP --global-cache-dir $TEMP --name ${package-name} -femit-bin=$out/bin/${package-name}
-        '';
+      with builtins;
+      with env.pkgs.lib;
+      rec {
+        # Produces clean binaries meant to be ship'd outside of nix
+        # nix build .#foreign
+        packages.foreign = env.package {
+          src = cleanSource ./.;
+
+          # Packages required for compiling
+          nativeBuildInputs = with env.pkgs; [ ];
+
+          # Packages required for linking
+          buildInputs = with env.pkgs; [ ];
+
+          # Smaller binaries and avoids shipping glibc.
+          zigPreferMusl = true;
+        };
+
+        # nix build .
+        packages.default = packages.foreign.override (attrs: {
+          # Prefer nix friendly settings.
+          zigPreferMusl = false;
+
+          # Executables required for runtime
+          # These packages will be added to the PATH
+          zigWrapperBins = with env.pkgs; [ ];
+
+          # Libraries required for runtime
+          # These packages will be added to the LD_LIBRARY_PATH
+          zigWrapperLibs = attrs.buildInputs or [ ];
+        });
+
+        # For bundling with nix bundle for running outside of nix
+        # example: https://github.com/ralismark/nix-appimage
+        apps.bundle = {
+          type = "app";
+          program = "${packages.foreign}/bin/default";
+        };
+
+        # nix run .
+        apps.default = env.app [ ] "zig build run -- \"$@\"";
+
+        # nix run .#build
+        apps.build = env.app [ ] "zig build \"$@\"";
+
+        # nix run .#test
+        apps.test = env.app [ ] "zig build test -- \"$@\"";
+
+        # nix run .#docs
+        apps.docs = env.app [ ] "zig build docs -- \"$@\"";
+
+        # nix run .#zig2nix
+        apps.zig2nix = env.app [ ] "zig2nix \"$@\"";
+
+        # nix develop
+        devShells.default = env.mkShell {
+          # Packages required for compiling, linking and running
+          # Libraries added here will be automatically added to the LD_LIBRARY_PATH and PKG_CONFIG_PATH
+          nativeBuildInputs = [
+            pkgs.zls
+          ]
+          ++ packages.default.nativeBuildInputs
+          ++ packages.default.buildInputs
+          ++ packages.default.zigWrapperBins
+          ++ packages.default.zigWrapperLibs;
+        };
       }
-    );
+    ));
 }
