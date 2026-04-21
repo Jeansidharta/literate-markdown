7c89b3739546be15b2529ca1ba06067125f79a65
j_sidharta <jeansidharta@gmail.com>
2026-04-21 10:33:44 -0300

    update to zig 0.16.0

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,33 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-    var stdargs = init.minimal.args.iterate();
+    var stdargs_iter = init.minimal.args.iterate();
 
+    const io = init.io;
     // Skip program name
-    _ = stdargs.next();
+    _ = stdargs_iter.next();
 
-    const inputFilePath = stdargs.next() orelse "-";
-    const outputFilePath = stdargs.next() orelse "-";
+    const inputFilePath = stdargs_iter.next() orelse "-";
+    const outputFilePath = stdargs_iter.next() orelse "-";
 
     var input_file = blk: {
         if (std.mem.eql(u8, inputFilePath, "-")) {
             break :blk std.Io.File.stdin();
         } else {
-            break :blk std.Io.Dir.cwd().openFile(init.io, inputFilePath, .{ .mode = .read_only }) catch |e| {
+            break :blk std.Io.Dir.cwd().openFile(io, inputFilePath, .{ .mode = .read_only }) catch |e| {
                 std.log.err("Failed to open input file {s}: {}", .{ inputFilePath, e });
                 return e;
             };
         }
     };
-    defer input_file.close(init.io);
+    defer input_file.close(io);
     var reader_buf: [16 * 1024]u8 = undefined;
-    var input_reader = input_file.reader(init.io, &reader_buf);
+    var input_reader = input_file.reader(io, &reader_buf);
     var input = &input_reader.interface;
 
     const output_file = blk: {
         if (std.fs.path.dirname(outputFilePath)) |parent| {
-            std.Io.Dir.cwd().createDirPath(init.io, parent) catch |e| {
+            std.Io.Dir.cwd().createDirPath(io, parent) catch |e| {
                 std.log.err("Failed to create path {s}: {}", .{ parent, e });
                 return e;
             };
@@ -34,16 +35,16 @@ pub fn main(init: std.process.Init) !void {
         if (std.mem.eql(u8, outputFilePath, "-")) {
             break :blk std.Io.File.stdout();
         } else {
-            break :blk std.Io.Dir.cwd().createFile(init.io, outputFilePath, .{}) catch |e| {
+            break :blk std.Io.Dir.cwd().createFile(io, outputFilePath, .{}) catch |e| {
                 std.log.err("Failed to open output file {s}: {}", .{ outputFilePath, e });
                 return e;
             };
         }
     };
-    defer output_file.close(init.io);
+    defer output_file.close(io);
 
     var output_buffer: [16 * 1024]u8 = undefined;
-    var output_writer = output_file.writer(init.io, &output_buffer);
+    var output_writer = output_file.writer(io, &output_buffer);
     const output = &output_writer.interface;
 
     var isCode = false;
